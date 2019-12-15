13c3ac7cf83f012e1bed41bb283674b4d90f1109
Abrikot <cirque.m@hotmail.fr>
2019-12-15 19:50:21 +0100

    Fix negative buffer

--- src/services.js
+++ src/services.js
@@ -214,7 +214,8 @@ function updateProsumer(prosumer, consumption, production, windSpeed, currentMar
         );
         prosumer.electricySentToTheMarket = productionConsumptionDifference * prosumer.productionRatioMarket;
     } else {
-        if (currentMarketElectricity > -productionConsumptionDifference * prosumer.consumptionRatioMarket) {
+        if (currentMarketElectricity > -productionConsumptionDifference * prosumer.consumptionRatioMarket
+        && prosumer.bufferFilling > -productionConsumptionDifference * prosumer.consumptionRatioBuffer) {
             prosumer.boughtElectricity = -productionConsumptionDifference * prosumer.consumptionRatioMarket;
             prosumer.bufferFilling += productionConsumptionDifference * prosumer.consumptionRatioBuffer;
         } else {
